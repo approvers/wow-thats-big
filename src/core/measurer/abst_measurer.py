a6517d9dcfadbdcb8bbbcb318da90926b4d228cf
loxygenk <loxygen.k@gmail.com>
2021-02-02 22:21:53 +0900

    🏷️ Defined AbstractMeasurer abstract class

---
+++ src/core/measurer/abst_measurer.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta, abstractmethod
+from typing import Union, TextIO, BinaryIO, Optional
+
+from src.core.type.argument import Argument
+from src.core.type.bigfileproperty import PartialBigFileProperty
+
+
+class AbstractMeasurer(metaclass=ABCMeta):
+    @abstractmethod
+    def measure(self, full_path: str, argument: Argument) -> Optional[PartialBigFileProperty]:
+        pass
