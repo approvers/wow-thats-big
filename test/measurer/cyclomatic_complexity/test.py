445eca1ea166a1867908a8737ff51f7ac94b7748
loxygenk <loxygen.k@gmail.com>
2021-02-04 01:39:36 +0900

    🚧 Work in progress, doing something

---
+++ test/measurer/cyclomatic_complexity/test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.measure import measure
+from src.type.argument import Argument
+
+
+class CoreTest(unittest.TestCase):
+
+    def test_measure(self):
+        arg = Argument(
+            directory="testcase",
+            min_line=100,
+            min_file_size_kb=512
+        )
+        big_files = measure(arg)
+        self.assertEqual(len(big_files), 3)
+        for file in big_files:
+            self.assertTrue("not_big" not in file.path and file.path.endswith("big"))
