5e2eee86907828d567a335a9842797774a4ee5a1
loxygenk <loxygen.k@gmail.com>
2021-02-06 10:42:17 +0900

    🐛 Completely forgot about zero division

--- src/measurer/measurers/cyclomatic_complexity.py
+++ src/measurer/measurers/cyclomatic_complexity.py
@@ -11,6 +11,10 @@ class CyclomaticComplexityMeasurer(AbstractMeasurer):
     def measure(self, full_path: str, argument: Argument) -> Optional[PartialBigFileProperty]:
         analyze_result: lizard.FileInformation = lizard.analyze_file(full_path)
         function_list: List[lizard.FunctionInfo] = analyze_result.function_list
+
+        if len(function_list) == 0:
+            return None
+
         avg_cyclocplx = sum([x.cyclomatic_complexity for x in function_list]) / len(function_list)
 
         if avg_cyclocplx > argument.min_cyclomatic_complexity:
