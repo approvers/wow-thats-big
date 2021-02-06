387e2d89de0edbc59281345dd7b0155ff44659d0
loxygenk <loxygen.k@gmail.com>
2021-02-06 11:22:08 +0900

    ✅ Replace Argument in test cases

--- test/measurer/filesize/test.py
+++ test/measurer/filesize/test.py
@@ -1,19 +1,24 @@
 import unittest
 
 from src.measurer.measurers.filesize import FileSizeMeasurer
-from src.type.argument import Argument
 
 
 class FileSizeMeasurerTest(unittest.TestCase):
 
     def test_no_big(self):
         measurer = FileSizeMeasurer()
-        result = measurer.measure("testcase/not_big", Argument(directory="", min_file_size_kb=512, min_line=-1))
+        result = measurer.measure(
+            "testcase/not_big",
+            {FileSizeMeasurer.MIN_FILE_SIZE_KB: 512}
+        )
         self.assertIsNone(result)
 
     def test_big(self):
         measurer = FileSizeMeasurer()
-        result = measurer.measure("testcase/big", Argument(directory="", min_file_size_kb=512, min_line=-1))
+        result = measurer.measure(
+            "testcase/big",
+            {FileSizeMeasurer.MIN_FILE_SIZE_KB: 512}
+        )
         self.assertIsNotNone(result)
         self.assertEqual(result.caption, "ファイルサイズ")
         self.assertEqual(result.info, "1.0 MB")
