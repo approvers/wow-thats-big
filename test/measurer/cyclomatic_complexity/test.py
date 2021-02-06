f4b8a52a82a2b3f828971524205e8efd0c82b97d
loxygenk <loxygen.k@gmail.com>
2021-02-06 10:21:54 +0900

    ✅ Wrote test code for CyclomaticComplexityMeasurer

---
+++ test/measurer/cyclomatic_complexity/test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.measurer.measurers.filesize import FileSizeMeasurer
+from src.type.argument import Argument
+
+
+class FileSizeMeasurerTest(unittest.TestCase):
+
+    def test_not_big(self):
+        measurer = FileSizeMeasurer()
+        result = measurer.measure("testcase/not_big.py", Argument(directory="", min_file_size_kb=512, min_line=-1))
+        self.assertIsNone(result)
+
+    def test_big(self):
+        measurer = FileSizeMeasurer()
+        result = measurer.measure("testcase/musical_typer_main.py", Argument(directory="", min_file_size_kb=512, min_line=-1))
+        self.assertIsNotNone(result)
+        self.assertEqual(result.caption, "循環的複雑度(平均)")
+        self.assertEqual(float(result.info), 18)
