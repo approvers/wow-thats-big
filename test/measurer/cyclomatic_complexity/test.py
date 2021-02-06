c3fb4c54e9a0ac34f751590a4f07bb47b70e240e
loxygenk <loxygen.k@gmail.com>
2021-02-06 10:25:35 +0900

    🏷️ Added "min_cyclomatic_complexity" field in Argument

--- test/measurer/cyclomatic_complexity/test.py
+++ test/measurer/cyclomatic_complexity/test.py
@@ -8,12 +8,28 @@ class FileSizeMeasurerTest(unittest.TestCase):
 
     def test_not_big(self):
         measurer = FileSizeMeasurer()
-        result = measurer.measure("testcase/not_big.py", Argument(directory="", min_file_size_kb=512, min_line=-1))
+        result = measurer.measure(
+            "testcase/not_big.py",
+            Argument(
+                directory="",
+                min_file_size_kb=512,
+                min_line=-1,
+                min_cyclomatic_complexity=15
+            )
+        )
         self.assertIsNone(result)
 
     def test_big(self):
         measurer = FileSizeMeasurer()
-        result = measurer.measure("testcase/musical_typer_main.py", Argument(directory="", min_file_size_kb=512, min_line=-1))
+        result = measurer.measure(
+            "testcase/musical_typer_main.py",
+            Argument(
+                directory="",
+                min_file_size_kb=512,
+                min_line=-1,
+                min_cyclomatic_complexity=15
+            )
+        )
         self.assertIsNotNone(result)
         self.assertEqual(result.caption, "循環的複雑度(平均)")
         self.assertEqual(float(result.info), 18)
