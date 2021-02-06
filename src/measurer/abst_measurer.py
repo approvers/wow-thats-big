00d0ba6d48f35b4876aae1918054e57e0b61f3d2
loxygenk <loxygen.k@gmail.com>
2021-02-06 10:50:44 +0900

    🏷️ Changed the way to receive arguments

--- src/measurer/abst_measurer.py
+++ src/measurer/abst_measurer.py
@@ -1,11 +1,14 @@
 from abc import ABCMeta, abstractmethod
-from typing import Optional
+from typing import Optional, Dict
 
-from src.type.argument import Argument
 from src.type.big_file_property import PartialBigFileProperty
 
 
 class AbstractMeasurer(metaclass=ABCMeta):
     @abstractmethod
-    def measure(self, full_path: str, argument: Argument) -> Optional[PartialBigFileProperty]:
+    def get_required_argument(self) -> Dict[str, type]:
+        pass
+
+    @abstractmethod
+    def measure(self, full_path: str, argument: Dict) -> Optional[PartialBigFileProperty]:
         pass
