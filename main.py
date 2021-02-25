def4007a7ce9c12e894daafafae6afc684a75c6e
loxygenk <loxygen.k@gmail.com>
2021-02-25 23:40:11 +0900

    ⚗️ show environment variable values for the test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("There is nothing yet, under developing")
+import os
+
+
+def main():
+    environment_keys = os.environ.keys()
+    max_length = max([len(x) for x in environment_keys])
+
+    print("--- Provided Environment Variables List ---")
+    for env in os.environ.keys():
+        print(f"  {env}{max_length - len(env)} : {os.environ[env]}")
+
+
+if __name__ == '__main__':
+    main()
