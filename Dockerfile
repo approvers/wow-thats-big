f88c8d171578208a39627255f241261781f71517
flisan <loxygen.k@gmail.com>
2021-02-26 15:13:02 +0900

    Run on the Action (#6)
    * ⚗️ show environment variable values for the test
    
    * 🔧 Prepared to run application on the Docker
    
    * ✏️ Use correct file name
    
    * 👷 Create a CI for test
    
    * 🐛 Fix invalid indenting code
    
    * ✨ Set configuration via environment variables
    
    * ✨ Run measurer via main.py
    
    * 🔧 Specified directory to scan
    
    * 🐛 Install dependencies before executing
    
    * 🐛 Use Pipfile
    
    * 🐛 Install dependencies to system python
    
    * 🐛 Copy necessary file to use "--system" option
    
    * ✨ Convert type of argument from environment variables dynamically
    
    * 🐛 Pass specified directory
    
    * 💬 Improved result text
    
    * ✨ Skip directories starting with "."
    
    * ✨ Generate report text
    
    * ✨ Expand detail automatically
    
    * ➕ Add octokit
    
    * ⚗️ Research what environment variables look like in PR
    
    * 👷 Enable CI when the pull_request is syncronized
    
    * ➕ Use correct package
    
    * ✨ Comment to the pull request
    
    * 🔥 Remove invalid description
    
    * ✨ Post request directly instead of using octokit.py
    
    * ⚗️ Check what event looks like
    
    * 🐛 Use JSON to post requests
    
    * ➕ Add requests to the dependency
    
    * 🐛 Use correct event name

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9-buster
+
+COPY main.py /app/main.py
+COPY Pipfile /app/Pipfile
+COPY Pipfile.lock /app/Pipfile.lock
+COPY src /app/src
+
+WORKDIR /app
+
+RUN pip3 install pipenv
+RUN pipenv install --system
+ENTRYPOINT ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,1 +1,87 @@
-print("There is nothing yet, under developing")
+import json
+import os
+import sys
+
+import requests
+
+from src import measure
+from src.report import generate_report_message
+
+
+def comment_to_github(text):
+    try:
+        token = os.environ["INPUT_TOKEN"]
+        event = os.environ["GITHUB_EVENT_NAME"]
+        api_root = os.environ["GITHUB_API_URL"]
+
+        owner, repo = os.environ["GITHUB_REPOSITORY"].split("/")
+        commit = "--- pull request ---"
+        pr = -1
+        if event == "push":
+            commit = os.environ["GITHUB_SHA"]
+        elif event == "pull_request":
+            pr = int(os.environ["GITHUB_REF"].split("/")[2])
+    except KeyError:
+        raise RuntimeError("Necessary environment variables were unavailable; is here in GitHub Actions?")
+
+    header = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    if event == "push":
+        res = requests.post(
+            f"{api_root}/repos/{owner}/{repo}/commits/{commit}/comments",
+            json.dumps({"body": text}),
+            headers=header
+        )
+    elif event == "pull_request":
+        res = requests.post(
+            f"{api_root}/repos/{owner}/{repo}/issues/{pr}/comments",
+            json.dumps({"body": text}),
+            headers=header
+        )
+    else:
+        res = None
+        print("Unknown event; skipping request.")
+
+    if res is not None and not str(res.status_code).startswith("2"):
+        print(f"Failed to post request: {res.status_code}")
+        print(res.text)
+    else:
+        print("Posted request successfully!")
+
+
+def main():
+
+    if "GITHUB_WORKSPACE" in os.environ:
+        root_dir = os.environ["GITHUB_WORKSPACE"]
+    else:
+        root_dir = os.curdir
+
+    scanning_directory = root_dir
+    if len(sys.argv) > 1:
+        scanning_directory += os.sep + sys.argv[1]
+
+    scanning_directory += os.sep
+
+    if not os.path.isdir(scanning_directory):
+        raise RuntimeError(f"The provided root directory is not found or directory: {scanning_directory}")
+
+    argument_table = {k.lower(): os.environ[k] for k in os.environ if not k.lower().startswith("github")}
+    result = measure.measure(scanning_directory, argument_table)
+
+    open_auto = True
+    if len(sys.argv) > 2 and sys.argv[2].lower() in ["true", "false"]:
+        open_auto = (sys.argv[2].lower() == "true")
+
+    text = generate_report_message(root_dir, result, open_auto)
+    if "CI" in os.environ and os.environ["CI"] == "true":
+        comment_to_github(text)
+    else:
+        print()
+        print(text)
+
+
+if __name__ == '__main__':
+    main()
