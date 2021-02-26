2e2026b477b4bb5e55599b8d6bae1c524d1bd283
loxygenk <loxygen.k@gmail.com>
2021-02-26 14:28:46 +0900

    ✨ Comment to the pull request

--- main.py
+++ main.py
@@ -4,17 +4,33 @@ import sys
 from src import measure
 from src.report import generate_report_message
 
+from octokit import Octokit
+
+
+def comment_to_github(text):
+    try:
+        token = os.environ["INPUT_TOKEN"]
+        event = os.environ["GITHUB_EVENT_NAME"]
+
+        owner, repo = os.environ["GITHUB_REPOSITORY"].split("/")
+        commit = "--- pull request ---"
+        pr = -1
+        if event == "commit":
+            commit = os.environ["GITHUB_SHA"]
+        elif event == "pull_request":
+            pr = int(os.environ["GITHUB_REF"].split("/")[2])
+    except KeyError:
+        raise RuntimeError("Necessary environment variables were unavailable; is here in GitHub Actions?")
+
+    kit = Octokit(token=token)
+
+    if event == "commit":
+        kit.repos.create_commit_comment(owner=owner, repo=repo, commit_sha=commit, body=text)
+    elif event == "pull_request":
+        kit.issues.create_comment(owner=owner, repo=repo, issue_number=pr, body=text)
+
 
 def main():
-
-    environment_keys = os.environ.keys()
-    max_length = max([len(x) for x in environment_keys])
-
-    print("--- Provided Environment Variables List ---")
-    for env in os.environ.keys():
-        print(f"  {env}{' ' * (max_length - len(env))} : {os.environ[env]}")
-
-    return
 
     if "GITHUB_WORKSPACE" in os.environ:
         root_dir = os.environ["GITHUB_WORKSPACE"]
